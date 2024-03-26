2fb5e891a650e8dec9e60c0591f84fd5cf4447b1
thedigialex <132579975+thedigialex@users.noreply.github.com>
2024-03-25 22:10:14 -0400

    Update nasa_pixelated_image.py
    Added a check in case image of the day is not an image

--- nasa_pixelated_image.py
+++ nasa_pixelated_image.py
@@ -8,8 +8,15 @@ def fetch_nasa_image(api_key):
     url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': api_key}
     response = requests.get(url, params=params)
-    image_url = response.json().get('url')
+    data = response.json()
+    image_url = data.get('url')
+    media_type = data.get('media_type')
+    
+    if media_type != 'image':
+        raise ValueError('The APOD for today is not an image.')
+    
     return image_url
+
 
 def pixelate_image(image_url, pixel_size=9):
     """Pixelate the image from the given URL and return both original and pixelated images."""
